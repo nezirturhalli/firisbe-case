6edce14e916e54cfb0b21aead35bb39f6d20a313
Nezir Turhallı <nzr.trhll@gmail.com>
2023-08-14 00:59:52 +0300

    Delete firisbecase directory

--- firisbecase/case/src/main/java/com/example/demo/constant/ErrorMessageConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.constant;
-
-public class ErrorMessageConstants {
-    public static final String CUSTOMER_NOT_FOUND = "Customer not found. Please check your customer id.";
-    public static final String EMAIL_ALREADY_EXIST = "Customer with the same email already exists.";
-    public static final String AMOUNT_MUST_BE_GREATER_ZERO = "Payment amount must be greater than 0\".";
-}
-
